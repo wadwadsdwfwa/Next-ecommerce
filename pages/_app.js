f8836e765907b7c19366171af1ba2ed2d3fc7d48
wadwadsdwfwa <tskim1880@gmail.com>
2022-11-22 09:48:07 +0900

    refresh

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,30 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <StoreProvider>
-        <Component {...pageProps} />
+        {Component.auth ? (
+          <Auth>
+            <Component {...pageProps} />
+          </Auth>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </StoreProvider>
     </SessionProvider>
   )
+}
+
+function Auth({ children }) {
+  const router = useRouter()
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/unauthorized?message=login required")
+    },
+  })
+  if (status === "loading") {
+    return <div>Loading...</div>
+  }
+  return children
 }
 
 export default MyApp
